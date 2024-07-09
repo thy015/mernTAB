bb6a5e46f723c1a329fff0e235625b3155914dd9
thy <thymai.1510@gmail.com>
2024-07-09 23:25:15 +0700

    .

--- server/src/routes/signUp/signUp.model.js
+++ server/src/routes/signUp/signUp.model.js
@@ -1,1 +1,13 @@
 const mongoose=require('mongoose')
+const signUpSchema=new mongoose.Schema(
+    {
+        name:{type:String,required:true},
+        passWord:{type:String,required:true},
+        isAdmin:{type:Boolean,required:true,default:false},
+        email:{type:String,required:true,unique:true},
+        birthDate:{type:String,required:true},
+        phoneNum:{type:String,required:true}
+    }
+)
+const Account=mongoose.model('Account',signUpSchema)
+module.exports=Account
