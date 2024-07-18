b2642b130a17b377c2fc9d955db9f35b0928c3fc
quocanhhoclaptrinh <tranvuquocanh2023@gmail.com>
2024-07-19 01:48:45 +0700

    qanh push homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 
-import './App.css';
-
+import { HomePage } from './Pages/HomePage';
+import './index.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Something</h1>
-      </header>
+      <HomePage></HomePage>
     </div>
   );
 }
