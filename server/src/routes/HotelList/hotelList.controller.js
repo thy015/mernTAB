282680f9df6b0b7afd31e80acfd48f6e504df376
thy <thymai.1510@gmail.com>
2024-07-20 13:58:35 +0700

    change to bookRoomT

--- server/src/routes/HotelList/hotelList.controller.js
+++ server/src/routes/HotelList/hotelList.controller.js
@@ -5,7 +5,7 @@ const createHotel=async(req,res)=>{
         console.log("Request ownerID in controller:", req.ownerID);
         const {address,numberOfRooms,taxCode,companyName,nation,facilityName,businessType,scale,city}=req.body
         if(!address || !numberOfRooms || !taxCode || !companyName ||!nation ||!facilityName||!businessType||!scale||!city){
-            return res.status(400).json({message:'Input is required'})
+            return res.status(403).json({message:'Input is required'})
         }
         const result =await service.createHotel(req.body,req.ownerID)
         return res.status(201).json(result)
@@ -30,7 +30,7 @@ const createRoom = async (req, res) => {
     try {
         const { numberOfBeds, typeOfRoom, money, hotelID,capacity } = req.body;
         if (!numberOfBeds || !typeOfRoom || !money || !hotelID||!capacity) {
-            return res.status(400).json({ message: 'Input is required' });
+            return res.status(403).json({ message: 'Input is required' });
         }
         const result = await service.createRoom(req.body, hotelID);
         return res.status(201).json(result);
@@ -39,8 +39,32 @@ const createRoom = async (req, res) => {
         return res.status(500).json({ message: e });
     }
 }
+
+const searchHotel=async(req,res)=>{
+    try{
+    const { city, checkInDate, checkOutDate, numberOfPeople } = req.body;
+
+    if (!city || !checkInDate || !checkOutDate || !numberOfPeople) {
+        return res.status(403).json({ message: 'Input is required' });
+    }
+
+    const searchCriteria = {
+        city,
+        checkInDate,
+        checkOutDate,
+        numberOfPeople: parseInt(numberOfPeople),
+    };
+
+    const result = await service.searchHotel(searchCriteria);
+    return res.status(200).json({ status: 'OK', data: result });
+    }catch(e){
+        console.error("Error in searchHotels controller:");
+        return res.status(500).json({ message: e });
+    }
+}
 module.exports={
     createHotel,
     createRoom,
-    getHotelsByOwner
+    getHotelsByOwner,
+    searchHotel
 }
