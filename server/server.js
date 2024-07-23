0dda80ba040ed6d2eb142618c51614ff152be9a1
thy <thymai.1510@gmail.com>
2024-07-23 17:42:32 +0700

    delete app test v

--- server/server.js
+++ server/server.js
@@ -1,6 +1,40 @@
 const PORT=process.env.PORT || 4000;
-const app=require('./src/app')
-const http=require('http')
+const express=require('express');
+const cors=require('cors');
+const dotenv=require('dotenv')
+const bodyParser=require('body-parser')
+dotenv.config()
+const app=express();
+const mongoose=require('mongoose')
+const http=require('http');
+const HomeRouter = require('./src/routes/home/home.route');
+const DetailRouter = require('./src/routes/HotelDetail/detail.route');
+const HotelListRouter=require('./src/routes/HotelList/hotelList.route');
+const signUpCusRouter = require('./src/routes/signUp/signUpCus.route');
+const signUprouter = require('./src/routes/signUp/signUp.route');
+//always put first
+app.use(bodyParser.json());
+
+app.use(cors({
+    origin:process.env.CLIENT_ORIGIN || 'http://localhost:3000'
+}))
+app.use('/',HomeRouter)
+app.use('/detail',DetailRouter)
+app.use('/hotelList',HotelListRouter)
+app.use('/signUp',signUprouter)
+app.use('/signUpCus',signUpCusRouter)
+
+//mongo connect
+mongoose.connect(`mongodb+srv://thymai1510:${process.env.MONGO_DB}@cluster0.ibhghsi.mongodb.net/?appName=Cluster0`)
+.then(()=>{
+    console.log('Connect successfully')
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+app.use(express.static('public'))
+
 
 const server=http.createServer(app);
 
