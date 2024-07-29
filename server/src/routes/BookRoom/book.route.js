e914ed83602f1ba405ebc91ed644817c736ed94b
thy <thymai.1510@gmail.com>
2024-07-29 17:40:35 +0700

    route

--- server/src/routes/BookRoom/book.route.js
+++ server/src/routes/BookRoom/book.route.js
@@ -2,8 +2,9 @@ const express = require('express');
 const bookController=require('./bookRoom.controller');
 const {authenCusToken } = require('../../services/jwt');
 const bookRouter = express.Router();
-
+const services=require('../../services/services')
 bookRouter.get('/', bookController.getInvoicesWithReceipts);
 
 bookRouter.post('/bookRoom',authenCusToken, bookController.bookRoom);
+bookRouter.post('/completedTran',services.completedTran)
 module.exports = bookRouter;
