37d459c8858aef5fd43292e0bfd2d64a010dc321
thy <thymai.1510@gmail.com>
2024-07-29 17:42:50 +0700

    update route

--- server/src/routes/BookRoom/book.route.js
+++ server/src/routes/BookRoom/book.route.js
@@ -6,5 +6,5 @@ const services=require('../../services/services')
 bookRouter.get('/', bookController.getInvoicesWithReceipts);
 
 bookRouter.post('/bookRoom',authenCusToken, bookController.bookRoom);
-bookRouter.post('/completedTran',services.completedTran)
+bookRouter.post('/completedTran/:id',services.completedTran)
 module.exports = bookRouter;
