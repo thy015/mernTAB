9a1658fdf31d5acb719c4d9e97f8af66bdb1c48e
thy <thymai.1510@gmail.com>
2024-07-29 23:49:40 +0700

    test

--- server/src/services/services.js
+++ server/src/services/services.js
@@ -139,7 +139,7 @@ async function signUpCustomer(newCustomer) {
           phoneNumber: phoneNumber,
         }
       );
-      if (response.status === 200 || response.status === 201) {
+      if (response.status === 200) {
         resolve({
           status: "OK",
           message: "Successfully created customer",
