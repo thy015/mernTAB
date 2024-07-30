62f8cb6716d4029e1d5468853313cc45eeb8125c
PaoMultiverse <baot21278@gmail.com>
2024-07-30 14:51:07 +0700

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Manage from './component/Manage';
 import Login from './component/Login';
 import HomePage from './component/HomePage'
-import TabDetail from './component/TabDetail';
 export default function App() {
   return (
     <div className="App">
@@ -20,7 +19,6 @@ export default function App() {
           <Route path='/registerOwner' element={<Register/>}/>
           <Route path='/manage' element={<Manage/>} />
           <Route path='/login' element={<Login/>} />
-          <Route path='/tab' element={<TabDetail hotelId={"6699bedd39e854acc06adee9"}/>}/>
         </Routes>
       </Router>
     </div>
