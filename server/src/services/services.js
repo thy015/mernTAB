343846dd73e63f320ced14df3885cbc1f1ace613
thy <thymai.1510@gmail.com>
2024-07-30 21:23:55 +0700

    .

--- server/src/services/services.js
+++ server/src/services/services.js
@@ -312,7 +312,7 @@ const handleCancelRoom = async (req, res) => {
 
   console.log(reqCancelID, accept, adminID, orderId, transactionId);
 
-  if (!accept === undefined || !adminID) {
+  if (accept === undefined || !adminID) {
     return res.status(403).json({ status: 'BAD', message: 'Missing required fields' });
   }
 
